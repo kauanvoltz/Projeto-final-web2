cc5d429228176db9aca54002c7eb31936f60b15f
MatheusBogisch <matheusbogisch@gmail.com>
2022-08-04 20:59:35 -0300

    Veiculo DAO FEITO

--- src/Model/DAO/VeiculoDAO.php
+++ src/Model/DAO/VeiculoDAO.php
@@ -20,25 +20,33 @@ class VeiculoDAO implements DAO
 
     public function findOne($id)
     {
-        
+        $conexao = Conexao::getConexao();
+        $stmt = $conexao->query("select * from veiculo where id_veiculo = $id");
+        return $stmt->fetch(PDO::FETCH_ASSOC);
     }
     public function findAll()
     {
-        
+        $conexao = Conexao::getConexao();
+        $stmt = $conexao->query("select * from veiculo;");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     public function update($object)
     {
-        
+        $conexao = Conexao::getConexao();
+        $stmt = $conexao-> prepare('UPDATE veiculo SET placa=?, modelo=?, ano=?, cor=? WHERE id_veiculo=?');
+        $stmt->bindParam(1, $object->placa);
+        $stmt->bindParam(2, $object->modelo);
+        $stmt->bindParam(3, $object->ano);
+        $stmt->bindParam(4, $object->cor);
+        $stmt->bindParam(5, $object->id_veiculo);
+        return $stmt->execute();
     }
     public function delete($id)
     {
-        
+        $conexao = Conexao::getConexao();
+        $stmt = $conexao->prepare('delete from veiculo where id_veiculo = ?');
+        $stmt ->bindParam(1, $id);
+        return $stmt->execute();
     }
 
-    public function findID()
-    {
-        $conexao = Conexao::getConexao();
-        $result = $conexao ->query("SELECT max(id_veiculo) as id_veiculo FROM veiculo;");
-        return $result->fetch(PDO::FETCH_ASSOC);
-    }
 }
--- src/Model/Validacao.php
+++ src/Model/Validacao.php
@@ -13,4 +13,20 @@ class Validacao
     {
         return $numero > 0;
     }
+    public static function validarPlaca(string $placa)
+    {
+        return mb_strlen($placa) == 7;
+    }
+    public static function validarModelo(string $modelo)
+    {
+        return mb_strlen($modelo) > 0;
+    }
+    public static function validarAno(string $ano)
+    {
+        return mb_strlen($ano) == 4;
+    }
+    public static function validarCor(string $cor)
+    {
+        return mb_strlen($cor) > 4;
+    }
 }
