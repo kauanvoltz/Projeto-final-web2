92bdecfd5dc38b12151ae93ab4feef40ebc7f73b
MatheusBogisch <matheusbogisch@gmail.com>
2022-08-03 21:39:30 -0300

    modifiquei conexao model

--- src/Model/Conexao.php
+++ src/Model/Conexao.php
@@ -6,7 +6,21 @@ use PDO;
 
 class Conexao 
 {
+    private static PDO $conexao;
 
+    private function __construct()
+    {
+    }
+
+    public static function getConexao()
+    {
+        if (empty(self::$conexao)){
+            self::$conexao = new PDO(DNS, USER, PASSWORD, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            ]);
+        }
+        return self::$conexao;
+    }
    
 }
 
