7ef865a350f98b68085aa8ba6400adf8706da158
Guilherme-Santos-dev <guilhermeciadope@gmail.com>
2022-08-09 20:14:40 -0300

    finalizando o cliente controller

--- src/Controller/Cliente.php
+++ src/Controller/Cliente.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace APP\CPntroller;
+namespace APP\Controller;
 
 use APP\Model\Cliente;
 use APP\Model\Endereco;
@@ -45,6 +45,7 @@ function inserirCliente()
             message: 'Requisição inválida!'
         );
 
+        $nomeDoCliente = $_POST['nome'];
         $cpfDoCliente = $_POST['cpf'];
         $telefoneDoCliente = $_POST['telefone'];
         $enderecoDoCliente = $_POST['endereco'];
@@ -52,10 +53,13 @@ function inserirCliente()
         $cepDoEndereco = $_POST['cep'];
         $bairroDoEndereco = $_POST['bairro'];
         $cidadeDoEndereco = $_POST['cidade'];
-        $complementoDoENdereco = $_POST['complemento'];
+        $complementoDoEndereco = $_POST['complemento'];
 
         $error = array();
-
+        if(!Validacao::validarNome($nomeDoCliente)){
+            array_push($error, "O nome do cliente deve conter no mínimo 2 caracteres!");
+        }
+        
         if (!Validacao::validarCpf($cpfDoCliente)) {
             array_push($error, "O CPF do cliente deve conter 11 caracteres!");
         }
@@ -84,7 +88,7 @@ function inserirCliente()
             array_push($error, "A cidade deve conter no mínimo 5 caracteres!");
         }
 
-        if (!Validacao::validarComplemento($complementoDoENdereco)) {
+        if (!Validacao::validarComplemento($complementoDoEndereco)) {
             array_push($error, "O complemento deve conter no mínimo 3 caracteres!");
         }
 
@@ -95,6 +99,7 @@ function inserirCliente()
             );
         } else {
             $cliente = new Cliente(
+                nome:$nomeDoCliente,
                 cpf: $cpfDoCliente,
                 telefone: $telefoneDoCliente,
                 endereco: new Endereco(
@@ -103,7 +108,7 @@ function inserirCliente()
                     cep: $cepDoEndereco,
                     bairro: $bairroDoEndereco,
                     cidade: $cidadeDoEndereco,
-                    complemento: $complementoDoENdereco,
+                    complemento: $complementoDoEndereco,
                 ),
                 id: $id = 0
             );
@@ -164,5 +169,103 @@ function listarCliente()
         }
     }catch(PDOException $e){
         Redirect::redirect("Lamento, hoive um erro inseperado!", type:'erro');
+    }
+}
+
+function consultarCliente()
+{
+    if(empty($_GET['code'])){
+        Redirect::redirect(message: 'O código do cliente não foi informado!');
+    }
+    $code = $_GET['code'];
+    $dao = new ClienteDAO();
+    try{
+        $resultado = $dao->findOne($code);
+    }catch(PDOException $e){
+        Redirect::redirect("Lamento, houve um erro inesperado!", type:'error');
+    }
+
+    if($resultado) {
+        session_start();
+        $_SESSION['cliente_info'] = $resultado;
+        header("location:../View/form_edit_cliente.php");
+    }else{
+        Redirect::redirect(message: 'Lamento, não localizamos o cliente em nossa base de dados', type:'error');
+    }
+}
+
+function editarCliente()
+{
+    if(empty($_POST)){
+        Redirect::redirect(message:'Requisição inválida!', type:'error');
+    }
+
+    $code = $_POST['code'];
+    $nomeDoCliente = $_POST['nome'];
+    $cpfDoCliente = $_POST['cpf'];
+    $telefoneDoCliente = $_POST['telefone'];
+    $enderecoDoCliente = $_POST['endereco'];
+    $numeroDoEndereco = $_POST['numero'];        $cepDoEndereco = $_POST['cep'];
+    $bairroDoEndereco = $_POST['bairro'];
+    $cidadeDoEndereco = $_POST['cidade'];
+    $complementoDoEndereco = $_POST['complemento'];
+
+    $error = array();
+    if(!Validacao::validarNome($nomeDoCliente)){
+        array_push($error, "O nome do cliente deve conter no mínimo 2 caracteres!");
+    }
+
+    if (!Validacao::validarCpf($cpfDoCliente)) {
+        array_push($error, "O CPF do cliente deve conter 11 caracteres!");
+    }
+
+    if (!Validacao::validarTelefone($telefoneDoCliente)) {
+        array_push($error, "O telefone do cliente deve conter no mínimo 9 caracteres!");
+    }
+
+    if (!Validacao::validarEndereco($enderecoDoCliente)) {
+        array_push($error, "O endereço do cliente deve conter no mínimo 5 caracteres!");
+    }
+
+    if (!Validacao::validarNumero($numeroDoEndereco)) {
+        array_push($error, "O Número do endereço deve conter no mínimo 1 caracter!");
+    }
+
+    if (!Validacao::validarCep($cepDoEndereco)) {
+        array_push($error, "O CEP deve conter 7 caracteres!");
+    }
+
+    if (!Validacao::validarBairro($bairroDoEndereco)) {
+        array_push($error, "O bairro deve conter no mínimo 5 caracteres!");
+    }
+
+    if (!Validacao::validarCidade($cidadeDoEndereco)) {
+        array_push($error, "A cidade deve conter no mínimo 5 caracteres!");
+    }
+
+    if (!Validacao::validarComplemento($complementoDoEndereco)) {
+        array_push($error, "O complemento deve conter no mínimo 3 caracteres!");
+    }
+
+    $cliente = new Cliente(
+        nome: $nomeDoCliente,
+        cpf: $cpfDoCliente,
+        telefone: $telefoneDoCliente,
+        endereco: new Endereco(
+            endereco:$enderecoDoCliente,
+            numero:$numeroDoEndereco,
+            cep:$cepDoEndereco,
+            bairro:$bairroDoEndereco,
+            cidade:$cidadeDoEndereco,
+            complemento:$complementoDoEndereco,
+        ),
+        id:$code
+    );
+    $dao = new ClienteDAO();
+    $resultado = $dao->update($cliente);
+    if($resultado){
+        Redirect::redirect(message:'Cliente atualizado com sucesso!');
+    }else{
+        Redirect::redirect(message:['Não foi possível atualizar os dados do cliente']);
     }
 }
